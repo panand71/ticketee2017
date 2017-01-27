5e9487556fa9f3586292fc315f4d0aa5271c9f6a
Premila Anand <panand71@github.com>
2017-01-27 13:30:39 -0800

    added admin flag to user model and seeded the first admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+unless User.exists?(email: "admin@ticketee.com")
+	User.create!(email: "admin@tickete.com", password: "password", admin: true)
+end
+
+unless User.exists?(email: "viewer@tickete.com")
+	User.create!(email: "viewer@tickete.com", password: "password")
+end
+
+["Sublime Text 3", "Internet Explorer"].each do |name|
+  unless Project.exists?(name: name)
+    Project.create!(name: name, description: "A sample project about #{name}"
+     )
+end end
